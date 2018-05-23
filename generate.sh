8d814588a8e330a9d166d74dc751b174d5a36056
Weston Steimel <weston.steimel@gmail.com>
2018-05-24 00:15:31 +0100

    Just use boost form Alpine pacakge repo and remove v1.11 stuff.
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

---
+++ generate.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+ql_versions=( "$@" )
+if [ ${#ql_versions[@]} -eq 0 ]; then
+	ql_versions=( */ )
+fi
+
+ql_versions=( "${ql_versions[@]%/}" )
+
+# see http://stackoverflow.com/a/2705678/433558
+sed_escape_lhs() {
+	echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
+}
+sed_escape_rhs() {
+	echo "$@" | sed -e 's/[\/&]/\\&/g' | sed -e ':a;N;$!ba;s/\n/\\n/g'
+}
+
+alpine_versions=(3.4 3.5 3.6 3.7)
+
+for version in "${ql_versions[@]}"; do
+    echo "Generating Dockerfiles for QuantLib version ${version}."
+
+    for alpine_version in ${alpine_versions[@]}; do
+	mkdir -p ${version}/alpine/${alpine_version}
+        
+	sed -r \
+	    -e 's!%%TAG%%!'"${alpine_version}"'!g' \
+	    -e 's!%%QUANTLIB_VERSION%%!'"${version}"'!g' \
+            "Dockerfile-alpine.template" > "${version}/alpine/${alpine_version}/Dockerfile"
+	echo "Generated ${version}/alpine/${alpine_version}/Dockerfile"
+    done
+done
