db31f505e76cce32983ccc04e03fa4ea16552460
Weston Steimel <weston.steimel@gmail.com>
2018-06-21 11:22:51 +0100

    Checksum downloads
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- generate.sh
+++ generate.sh
@@ -17,9 +17,12 @@ sed_escape_rhs() {
 }
 
 alpine_versions=(3.6 3.7)
+declare -A ql_checksums
+ql_checksums=(["1.12"]="fcf82734fa065a81141a67f0fe185d80166e60199dd7143ac5847af4ce9a36ed" ["1.13"]="bb52df179781f9c19ef8e976780c4798b0cdc4d21fa72a7a386016e24d1a86e6")
 
 for version in "${ql_versions[@]}"; do
     echo "Generating Dockerfiles for QuantLib version ${version}."
+    ql_checksum=${ql_checksums[$version]}
 
     for alpine_version in ${alpine_versions[@]}; do
 	mkdir -p ${version}/alpine/${alpine_version}
@@ -27,6 +30,7 @@ for version in "${ql_versions[@]}"; do
 	sed -r \
 	    -e 's!%%TAG%%!'"alpine${alpine_version}"'!g' \
 	    -e 's!%%QUANTLIB_VERSION%%!'"${version}"'!g' \
+	    -e 's!%%QUANTLIB_CHECKSUM%%!'"$ql_checksum"'!g' \
             "Dockerfile-alpine.template" > "${version}/alpine/${alpine_version}/Dockerfile"
 	echo "Generated ${version}/alpine/${alpine_version}/Dockerfile"
     done
